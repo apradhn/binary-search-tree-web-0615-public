9d3b3841c6de41295ee06b57c2592e3aa29c4830
apradhn <abhijit.pradhan6@gmail.com>
2015-08-31 17:02:22 -0400

    refactors by reducing # of while loops

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -5,6 +5,8 @@ class BST
 
   def initialize(data)
     @data = data
+    @left = nil
+    @right = nil
   end
 
   def insert(element)
@@ -21,17 +23,17 @@ class BST
     left_node = self
     right_node = self
 
-    while left_node.left do 
-      left_node = left_node.left
-      sorted_list.unshift(left_node.right.data) if left_node.right
-      sorted_list.unshift(left_node.data)
-    end
-
-    
-    while right_node.right do 
-      right_node = right_node.right
-      sorted_list.push(right_node.left.data) if right_node.left
-      sorted_list.push(right_node.data)
+    while left_node.left || right_node.right do 
+      if right_node.right
+        right_node = right_node.right
+        sorted_list.push(right_node.left.data) if right_node.left
+        sorted_list.push(right_node.data) 
+      end
+      if left_node.left
+        left_node = left_node.left
+        sorted_list.unshift(left_node.right.data) if left_node.right
+        sorted_list.unshift(left_node.data)
+      end
     end
 
     sorted_list.each do |data|
